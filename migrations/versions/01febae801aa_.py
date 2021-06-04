1998ea9ac68d2a25d008176ba15cd64c89a0d7e5
Juan Manuel Valencia Zapata <jmvalenciz@protonmail.com>
2021-06-04 10:31:21 -0500

    First Commit

---
+++ migrations/versions/01febae801aa_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 01febae801aa
+Revises: d98e374435a6
+Create Date: 2021-04-23 19:43:39.895995
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '01febae801aa'
+down_revision = 'd98e374435a6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
